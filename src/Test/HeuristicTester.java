7ced78e05f67a281a948311a7cfe3c2610ca3c71
Ektedar <ektedar.alam@uconn.edu>
2017-04-21 15:49:05 -0400

    Testing the branch

---
+++ src/Test/HeuristicTester.java
@@ -0,0 +1,14 @@
+package Test;
+import java.util.Random;
+import java.util.Scanner;
+
+import Controller.AlphaBeta;
+import Model.CheckersGameState;
+import Model.ThirtyFiveRepBoard;
+
+public class HeuristicTester {
+	CheckersGameState cb = new ThirtyFiveRepBoard();
+	AlphaBeta test= new AlphaBeta();
+	Scanner input = new Scanner(System.in);
+	
+}
