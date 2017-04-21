5aca4a8aabc6381db6cf73cefb18cec6eab1b068
Ektedar <ektedar.alam@uconn.edu>
2017-04-21 18:32:38 -0400

    Tested functionality for cntrheuristic

--- src/Test/HeuristicTester.java
+++ src/Test/HeuristicTester.java
@@ -12,6 +12,7 @@ import Model.ThirtyFiveRepCheckerPiece;
 public class HeuristicTester {
 	public static void main(String[] args) {
 		centerTest();
+		cntTest();
 	}
 	
 	public static void centerTest() {
@@ -26,8 +27,8 @@ public class HeuristicTester {
 		set[19] = new ThirtyFiveRepCheckerPiece("White");
 		set[20] = new ThirtyFiveRepCheckerPiece("White");
 		
-//		set[23] = new ThirtyFiveRepCheckerPiece("Black");
-//		set[24] = new ThirtyFiveRepCheckerPiece("White");
+		set[23] = new ThirtyFiveRepCheckerPiece("Black");
+		set[24] = new ThirtyFiveRepCheckerPiece("White");
 		
 		ThirtyFiveRepBoard state = new ThirtyFiveRepBoard(set);
 		System.out.println("Center heuristic test for passive man");
@@ -38,6 +39,34 @@ public class HeuristicTester {
 		
 		System.out.print("Heuristic For White: ");
 		state.centheuristic("White");
+	}
+	
+	public static void cntTest() {
+		ThirtyFiveRepCheckerPiece[] set = new ThirtyFiveRepCheckerPiece[35];
+
+		set[10] = new ThirtyFiveRepCheckerPiece("White");
+		set[10].turnKing();
+		set[11] = new ThirtyFiveRepCheckerPiece("Black");
 		
+		set[14] = new ThirtyFiveRepCheckerPiece("Black");
+		set[14].turnKing();
+		set[15] = new ThirtyFiveRepCheckerPiece("White");
+		
+	//	set[19] = new ThirtyFiveRepCheckerPiece("Black");
+		set[20] = new ThirtyFiveRepCheckerPiece("White");
+		set[20].turnKing();
+		
+	//	set[23] = new ThirtyFiveRepCheckerPiece("Black");
+		set[24] = new ThirtyFiveRepCheckerPiece("Black");
+		
+		ThirtyFiveRepBoard state = new ThirtyFiveRepBoard(set);
+		System.out.println("Center heuristic test for active man");
+		state.printState();
+		
+		System.out.print("Heuristic For Black: ");
+		state.cntrheuristic("Black");
+		
+		System.out.print("Heuristic For White: ");
+		state.cntrheuristic("White");
 	}
 }
