d0b08a030a917e8a8de3f40af9d0e0dc4824393c
Ektedar <ektedar.alam@uconn.edu>
2017-04-21 17:41:23 -0400

    Made my heuristic class

--- src/Test/HeuristicTester.java
+++ src/Test/HeuristicTester.java
@@ -1,14 +1,56 @@
 package Test;
+
 import java.util.Random;
 import java.util.Scanner;
 
 import Controller.AlphaBeta;
 import Model.CheckersGameState;
+import Model.Move;
 import Model.ThirtyFiveRepBoard;
 
 public class HeuristicTester {
-	CheckersGameState cb = new ThirtyFiveRepBoard();
-	AlphaBeta test= new AlphaBeta();
-	Scanner input = new Scanner(System.in);
+	public static void main(String[] args) {
+//		CheckersGameState cb = new ThirtyFiveRepBoard();
+//		Scanner input = new Scanner(System.in);
+//		int choice, sz;
+//		
+//		System.out.println("Please note that error checks for bad user inputs are not implemented. Please follow the directions");
+//		
+//		int i = 0;
+//		int curr;
+//		while(i < 35){
+//			curr = i;
+//			while(i < curr + 4){
+//				System.out.print("-" + Integer.toString(i));
+//				i++;
+//			}
+//			System.out.println();
+//			curr = i;
+//			while(i < curr + 4){
+//				System.out.print(Integer.toString(i) + "-");
+//				i++;
+//			}
+//			i++;
+//			System.out.println();
+//		}	
+//		Random rand = new Random();
+//		cb.printState();
+//		while(!cb.isTerminal()){
+//			System.out.println("**********************************");
+//			if(!cb.actions().isEmpty()){
+//				sz = cb.actions().size();
+//				choice = rand.nextInt(sz);
+//				System.out.println(cb.actions().get(choice));
+//				cb = cb.result(cb.actions().get(choice));
+//			}
+//			cb.printState();
+//		}
+		centerTest();
+	}
 	
+	public static void centerTest() {
+		ThirtyFiveRepBoard tb = new ThirtyFiveRepBoard();
+		tb.printState();
+		
+	}
 }
